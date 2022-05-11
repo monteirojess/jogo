7f0d4a8785e52b1f9c8d342e737c50c6d1ae167f
monteirojess <jessicamonteiro1613@gmail.com>
2022-05-11 13:01:32 -0300

    First commit

---
+++ jogo da adivinhação.py
@@ -0,0 +1,20 @@
+from random import randint
+print('=' * 30, ' JOGO DE ADIVINHAÇÃO', '=' * 30)
+print('Olá, seja bem vindo(a) ! Escolha um número entre 1 e 100 e tente me derrotar !')
+cont = 0
+usuario = 0
+
+computador = randint(1, 100)
+while usuario != computador:
+    usuario = int(input('Digite um número:'))
+    cont += 1
+    if usuario < computador:
+        print('ERROU !!')
+        print('Seu palpite está abaixo do computador. Tente mais uma vez !')
+    elif usuario > computador:
+        print('ERROU !!')
+        print('Vish ... foi longe. Vamos mais uma vez !')
+    else:
+        print('Você acertou, parabéns. Você tentou {}  vezes me derrotar.'.format(cont))
+        print('=' * 30, 'FIM DE JOGO',  '=' * 30)
+
